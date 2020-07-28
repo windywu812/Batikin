54c8db905d9f8b3883d5f6a98d0760fb1457950c
Windy <windywu812@gmail.com>
2020-07-28 10:52:01 +0700

    Initial Commit

---
+++ Batikin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Batikin
+//
+//  Created by Windy on 28/07/20.
+//  Copyright © 2020 BatikAja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
