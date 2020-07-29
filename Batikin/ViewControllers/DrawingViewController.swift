34e4172a83299ef94f2ba0590001c0358ffc797c
Windy <windywu812@gmail.com>
2020-07-29 09:26:45 +0700

    Setup project

---
+++ Batikin/ViewControllers/DrawingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DrawingViewController.swift
+//  Batikin
+//
+//  Created by Windy on 29/07/20.
+//  Copyright © 2020 BatikAja. All rights reserved.
+//
+
+import UIKit
+
+class DrawingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Batikin/ViewControllers/GalleryViewController.swift
+++ Batikin/ViewControllers/GalleryViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GalleryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
