0e8e782c2b5f6a32560f7b1148c18147a60e6755
Windy <windywu812@gmail.com>
2020-08-08 14:36:39 +0700

    add color

--- Batikin/Supports/Constant.swift
+++ Batikin/Supports/Constant.swift
@@ -11,6 +11,7 @@ import Foundation
 struct Constant {
     static let collectionCell = "collectionCell"
     static let headerCell = "headerCell"
+    static let shapeCollectionViewCell = "ShapeCollectionViewCell"
 }
 
 var imageArray = ["motif_flower_a","motif_flower_b","motif_leaf_a","motif_leaf_b","motif_leaf_b","motif_leaf_b","motif_leaf_b","motif_leaf_b","motif_leaf_b"]
